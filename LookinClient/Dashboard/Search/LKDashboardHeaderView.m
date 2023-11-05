c8568f353541bcf6a23964a79fc8b19b15e66d99
likai.123 <471633877@qq.com>
2023-11-05 22:39:17 +0800

    custom attr

--- LookinClient/Dashboard/Search/LKDashboardHeaderView.m
+++ LookinClient/Dashboard/Search/LKDashboardHeaderView.m
@@ -11,21 +11,28 @@
 
 @interface LKDashboardHeaderView () <NSTextFieldDelegate>
 
+@property(nonatomic, strong) NSView *inputBorderView;
 @property(nonatomic, strong) NSImageView *iconImageView;
 @property(nonatomic, strong) NSTextField *textField;
+@property(nonatomic, strong) NSButton *addButton;
 
 @end
 
 @implementation LKDashboardHeaderView {
     CGFloat _iconXWhenActive;
+    CGFloat _iconXWhenInactive;
 }
 
 - (instancetype)initWithFrame:(NSRect)frameRect {
     if (self = [super initWithFrame:frameRect]) {
         _iconXWhenActive = 11;
+        _iconXWhenInactive = 89;
         
-        self.layer.cornerRadius = DashboardCardCornerRadius;
-        self.layer.borderWidth = 1;
+        self.inputBorderView = [NSView new];
+        self.inputBorderView.wantsLayer = YES;
+        self.inputBorderView.layer.cornerRadius = DashboardCardCornerRadius;
+        self.inputBorderView.layer.borderWidth = 1;
+        [self addSubview:self.inputBorderView];
         
         self.iconImageView = [NSImageView new];
         self.iconImageView.image = NSImageMake(@"icon_search");
@@ -45,6 +52,14 @@
         self.textField.hidden = YES;
         [self addSubview:self.textField];
         
+        self.addButton = [NSButton new];
+        self.addButton.image = [NSImage imageNamed:NSImageNameAddTemplate];
+        self.addButton.bezelStyle = NSBezelStyleRounded;
+        self.addButton.target = self;
+        self.addButton.action = @selector(handleAddButton);
+        self.addButton.frame = NSMakeRect(0, 0, 84, 40);
+        [self addSubview:self.addButton];
+        
         @weakify(self);
         [[self.textField.rac_textSignal throttle:0.3] subscribeNext:^(NSString * _Nullable x) {
             @strongify(self);
@@ -61,9 +76,12 @@
 - (void)layout {
     [super layout];
     if (self.isActive) {
+        $(self.inputBorderView).fullFrame;
         $(self.iconImageView).sizeToFit.x(_iconXWhenActive);
     } else {
-        $(self.iconImageView).sizeToFit.centerAlign;
+        $(self.addButton).width(50).fullHeight.right(-6).offsetY(1);
+        $(self.inputBorderView).x(0).toRight(48).fullHeight;
+        $(self.iconImageView).sizeToFit.verAlign.x(_iconXWhenInactive);
     }
     $(self.textField).x(30).toRight(2).heightToFit.verAlign;
 }
@@ -71,9 +89,9 @@
 - (void)updateColors {
     [super updateColors];
     if (self.isActive) {
-        self.layer.borderColor = self.isDarkMode ? LookinColorMake(70, 71, 72).CGColor : LookinColorMake(198, 199, 200).CGColor;
+        self.inputBorderView.layer.borderColor = self.isDarkMode ? LookinColorMake(70, 71, 72).CGColor : LookinColorMake(198, 199, 200).CGColor;
     } else {
-        self.layer.borderColor = self.isDarkMode ? LookinColorMake(47, 48, 49).CGColor : LookinColorMake(220, 221, 222).CGColor;
+        self.inputBorderView.layer.borderColor = self.isDarkMode ? LookinColorMake(47, 48, 49).CGColor : LookinColorMake(220, 221, 222).CGColor;
     }
 }
 
@@ -95,13 +113,17 @@
     [self updateColors];
     
     if (isActive) {
+        [self.addButton.animator setAlphaValue:0];
+        [self.inputBorderView.animator setFrameSize:self.$size];
         [self.iconImageView.animator setFrameOrigin:NSMakePoint(_iconXWhenActive, self.iconImageView.$y)];
         
         self.textField.animator.hidden = NO;
         [self.textField becomeFirstResponder];
         
     } else {
-        [self.iconImageView.animator setFrameOrigin:NSMakePoint(self.$width / 2.0 - self.iconImageView.$width / 2.0, self.iconImageView.$y)];
+        [self.addButton.animator setAlphaValue:1];
+        [self.inputBorderView.animator setFrameSize:NSMakeSize(self.$width - 48, self.$height)];
+        [self.iconImageView.animator setFrameOrigin:NSMakePoint(_iconXWhenInactive, self.iconImageView.$y)];
         
         self.textField.animator.hidden = YES;
         self.textField.stringValue = @"";
@@ -110,6 +132,23 @@
     if ([self.delegate respondsToSelector:@selector(dashboardHeaderView:didToggleActive:)]) {
         [self.delegate dashboardHeaderView:self didToggleActive:isActive];
     }
+}
+
+- (void)handleAddButton {
+    NSMenu *menu = [NSMenu new];
+    
+    NSMenuItem *menuItem = [NSMenuItem new];
+    menuItem.image = NSImageMake(@"Icon_Inspiration_small"); //[[NSImage alloc] initWithSize:NSMakeSize(1, 22)];
+    menuItem.title = NSLocalizedString(@"How to add custom properties…", nil);
+    menuItem.target = self;
+    menuItem.action = @selector(handleAddCustomAttr);
+    [menu addItem:menuItem];
+    
+    [NSMenu popUpContextMenu:menu withEvent:NSApplication.sharedApplication.currentEvent forView:self.addButton];
+}
+
+- (void)handleAddCustomAttr {
+    [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"https://bytedance.feishu.cn/docx/TRridRXeUoErMTxs94bcnGchnlb"]];
 }
 
 #pragma mark - <NSTextFieldDelegate>
